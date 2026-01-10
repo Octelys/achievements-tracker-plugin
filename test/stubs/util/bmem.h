9bf0a2f1a45ab1cdebbeef39523b17ef06b959c3
kzryzstof <kzryzstof@proton.me>
2026-01-10 09:29:49 -0500

    Add stub implementation for bmem functions and unit tests for base64 encoding

---
+++ test/stubs/util/bmem.h
@@ -0,0 +1,13 @@
+/*
+ * test/stubs/bmem.h
+ *
+ * Stub header for OBS bmem functions for unit testing.
+ */
+
+#pragma once
+
+#include <stddef.h>
+
+void *bzalloc(size_t size);
+void bfree(void *ptr);
+
