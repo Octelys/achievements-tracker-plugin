dfa2e56331d36b3a69f43037dc9b463b9e51f60e
kzryzstof <kzryzstof@proton.me>
2026-02-07 20:19:08 -0500

    Refactor: replace hardcoded text alignment values with constants for improved readability

--- src/sources/xbox/achievement_name.c
+++ src/sources/xbox/achievement_name.c
@@ -300,8 +300,8 @@ static obs_properties_t *source_get_properties(void *data) {
     // Text alignment dropdown.
     obs_property_t *align_list =
         obs_properties_add_list(p, "text_align", "Text alignment", OBS_COMBO_TYPE_LIST, OBS_COMBO_FORMAT_INT);
-    obs_property_list_add_int(align_list, "Left", 0);
-    obs_property_list_add_int(align_list, "Right", 1);
+    obs_property_list_add_int(align_list, "Left", TEXT_ALIGN_LEFT);
+    obs_property_list_add_int(align_list, "Right", TEXT_ALIGN_RIGHT);
 
     return p;
 }
