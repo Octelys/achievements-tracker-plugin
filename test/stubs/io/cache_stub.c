e3d437c5e674e2d21dc73ee4692c31480dc85f00
kzryzstof <kzryzstof@proton.me>
2026-02-22 15:35:28 -0500

    Add `cache_stub` to test stubs for `io/cache` module

---
+++ test/stubs/io/cache_stub.c
@@ -0,0 +1,23 @@
+#include "io/cache.h"
+
+#include "io/cache.h"
+
+#include <stdbool.h>
+#include <stddef.h>
+
+void cache_build_path(const char *type, const char *id, char *out_path, size_t path_size) {
+    (void)type;
+    (void)id;
+    (void)out_path;
+    (void)path_size;
+}
+
+bool cache_download(const char *url, const char *type, const char *id, char *out_path, size_t path_size) {
+    (void)url;
+    (void)type;
+    (void)id;
+    (void)out_path;
+    (void)path_size;
+    return true;
+}
+
